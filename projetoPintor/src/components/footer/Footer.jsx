5a1a5d916e91d75d5d1f0a3705105ef12e135ad8
ThiagoVanucci <thiago.dev24@gmail.com>
2025-06-03 15:24:56 -0300

    footer ajustado

--- projetoPintor/src/components/footer/Footer.jsx
+++ projetoPintor/src/components/footer/Footer.jsx
@@ -1,26 +1,45 @@
 import React from "react";
+import "bootstrap-icons/font/bootstrap-icons.css";
 
 const Footer = () => {
   return (
     <footer className="bg-dark text-white py-4 mt-5">
       <div className="container">
-        {/* Texto centralizado dos direitos autorais */}
         <div className="row">
-          <div className="col-12 text-center mb-2">
+          {/* Coluna Desenvolvedores */}
+          <div className="col-3 text-center mb-2">
+            <div className="text-center text-md-start small">
+              <span className="fw-bold text-warning">Desenvolvedores</span>
+            </div>
+            <p className="mb-1 text-md-start">
+              <a
+                href="https://github.com/ThiagoVanucci01"
+                className="text-warning text-decoration-none"
+              >
+                <i className="bi bi-github me-1"></i>Thiago Vanucci
+              </a>
+              <br />
+              <a
+                href="https://github.com/Tiagop244"
+                className="text-warning text-decoration-none"
+              >
+                <i className="bi bi-github me-1"></i>Tiago Pereira
+              </a>
+            </p>
+          </div>
+
+          {/* Coluna Direitos Autorais */}
+          <div className="col-6 text-center">
             <p className="mb-0">
               © 2025 Pinturas Tiago Duarte. Todos os direitos reservados.
             </p>
           </div>
 
-          {/* "Fale Conosco" em uma linha separada */}
-
-          <div className="col-12 text-center text-md-end small mb-1">
-            <span className="fw-bold text-warning">Fale Conosco</span>
-          </div>
-
-          {/* Telefone e Email abaixo, também alinhados à direita em telas maiores */}
-
-          <div className="col-12 text-center text-md-end small">
+          {/* Coluna Fale Conosco */}
+          <div className="col text-center text-md-end small">
+            <div className="text-center text-md-end ">
+              <span className="fw-bold text-warning">Fale Conosco</span>
+            </div>
             <p className="mb-1">
               📞{" "}
               <a
